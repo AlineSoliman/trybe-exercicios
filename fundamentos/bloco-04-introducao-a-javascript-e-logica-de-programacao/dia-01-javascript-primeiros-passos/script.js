882d98f7007653ebdd02d87da9b914e88514e668
Aline Soliman <alineapreis@hotmail.com>
2022-02-02 18:26:28 -0300

    first commit

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,48 @@
+const myName = "Aline Soliman";
+const birthCity = "Santa Isabel";
+let birthYear = 1988;
+console.log(myName);
+birthYear = 2030 // tipagem dinâmica
+console.log(birthYear)
+
+
+
+const comida = 'pão na chapa';
+const bebida = 'cafézinho';
+
+if (bebida === 'cafézinho' && comida === 'pão na chapa') {
+  console.log('Muito obrigado pela refeição :)');
+} else {
+  console.log('Acho que houve um engano com meu pedido');
+}
+
+const conditionOne = true;
+const conditionTwo = false;
+
+console.log(conditionOne && conditionTwo);
+
+
+
+let a = 15;
+let b = 20;
+
+let adição = (a+b);
+console.log(adição)
+
+let subtração = (a-b);
+console.log(subtração)
+
+let multiplicação = (a*b);
+console.log(multiplicação)
+
+let divisão = (a/b);
+console.log(divisão)
+
+let módulo = (a%b);
+console.log(módulo)
+
+
+
+
+
+
