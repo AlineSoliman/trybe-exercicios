c19cf7a18546b7a8a3fb881a25a7f95cafa9a535
Aline Soliman <alineapreis@hotmail.com>
2022-02-04 11:33:34 -0300

    inclusão de exercícios

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/JSdia2.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/JSdia2.js
@@ -101,5 +101,23 @@ console.log(numero);
 // O laço for/of permite iterar os valores das propriedades, nos retornando os números dentro do array numeros
 // // Vamos para outro exemplo:
 
+let word = 'Hello';
+for (let letter of word) {
+console.log(letter);
+}
 
+// Por fim, com o for/of , nós conseguimos somar um valor a cada elemento do array e retorná-los da seguinte forma:
+let arrOfNumbers = [10, 20, 30];
+for (let sum of arrOfNumbers) {
+sum += 1;
+console.log(sum);
+}
+// É importante ressaltar que o for/of não irá alterar o array, e sim trazer os valores de dentro, e no caso, adicionando 1 à soma.
+console.log(arrOfNumbers);
 
+// Utilize o for/of para imprimir os elementos da lista names com o console.log() :
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let elemento of names)
+console.log(elemento);
