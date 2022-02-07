66105768b2bcaa783ee48112a6e1714081d22d11
Aline Soliman <alineapreis@hotmail.com>
2022-02-07 18:52:01 -0300

    exercícios de fixação aula 4.4 JS for/in

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/JSdia4.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/JSdia4.js
@@ -114,3 +114,15 @@ console.log("A jogadora "+player.name +' '+ player.lastName + " foi eleita a mel
 // 5 - Acesse a chave medals e faça um console.log no seguinte formato: "A jogadora possui 2 medalhas de ouro e 3 medalhas de prata".
 
 console.log("A jogadora possui "+ player.medals.golden + " medalhas de ouro e "+ player.medals.silver + " medalhas de prata.");
+
+// 1 - Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+};
+
+for (let index in names) {
+    console.log("Olá", names[index]);
+}
