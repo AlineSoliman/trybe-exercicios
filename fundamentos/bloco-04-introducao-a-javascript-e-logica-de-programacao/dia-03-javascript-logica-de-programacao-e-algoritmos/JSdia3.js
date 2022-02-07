1af86ee10a034f2cf8e0aa57f1495e7b7def54e0
Aline Soliman <alineapreis@hotmail.com>
2022-02-07 15:19:31 -0300

    inclusão exercício 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/JSdia3.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/JSdia3.js
@@ -5,4 +5,18 @@ let fatorial = 1;
 for(let i=1; i<=10; i+=1){
 fatorial = fatorial*i;
 }
-console.log(fatorial);
+console.log(fatorial); // console.log fora do for
+
+
+// 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+let word = reverse('tryber');
+let soma ;
+
+// for(let i = 0; i< word.length;i-=1){
+//     // console.log(word[i]);
+//     soma = word-i;
+   
+// }
+// console.log(soma);
+console.log(word);
