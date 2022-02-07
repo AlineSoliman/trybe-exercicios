5a1ef87c88770337cae45425ebdb96aa8c669777
Aline Soliman <alineapreis@hotmail.com>
2022-02-07 19:52:50 -0300

    Inclusão do exercícios 4

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/JSdia4.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/JSdia4.js
@@ -160,5 +160,24 @@ console.log(info);
 for(let index in info){
     console.log(index);
 }
+// 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+for(let index in info){
+    console.log(info[index])
+}
 
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
 
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+};
+
+for(let index in info){
+    for(let index in info2)
+    console.log(info[index] + " "+info2[index])
+    if (info.recorrente == 'Sim'&& info2.recorrente == 'Sim'){
+        console.log("Ambos recorrentes");
+}
+};
