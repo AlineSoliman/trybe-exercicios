af2ed28a3a5d7480df8c4307753197af5bdf76e6
Aline Soliman <alineapreis@hotmail.com>
2022-02-08 13:37:55 -0300

    Inclusão do exercícios 8 e 9

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/JSdia2.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/JSdia2.js
@@ -192,3 +192,17 @@ if(numbers[i]<menorNumero){
 }
 }
 console.log(menorNumero);
+
+// Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+let array = [];
+for (let index = 1; index <=25; index+=1){
+    array.push(index)
+}
+console.log(array);
+
+
+// Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+for (let index = 1; index < array.length; index+=1) {
+    console.log(array[index]/2);
+}
