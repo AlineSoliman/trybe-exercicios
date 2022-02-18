8fc585c724f058cf8c333a27594c10de49efa7be
Aline Soliman <alineapreis@hotmail.com>
2022-02-18 15:41:20 -0300

    para fixar

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,15 @@
+// SELETORES
+const INPUT_TEXT = document.querySelector("#input-text");
+INPUT_TEXT.addEventListener('keypress', )
+
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+INPUT_CHECKBOX.addEventListener('click', check);
+function check(event){
+    event.preventDefault();  
+}
+
+const HREF_LINK = document.querySelector("#href");
+HREF_LINK.addEventListener('click', clica);
+function clica(event){
+    event.preventDefault();
+}
