238f8e607b0f9b8557c1bd7b9a712887d5e51142
Aline Soliman <alineapreis@hotmail.com>
2022-02-18 17:42:14 -0300

    exercício formulário

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,15 +1,18 @@
 // SELETORES
 const INPUT_TEXT = document.querySelector("#input-text");
-INPUT_TEXT.addEventListener('keypress', )
+INPUT_TEXT.addEventListener('keypress', (event) => {
+    const character = event.key;
+    if (character !== 'a') {
+      event.preventDefault();
+    }
+});
 
 const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
-INPUT_CHECKBOX.addEventListener('click', check);
-function check(event){
-    event.preventDefault();  
-}
+INPUT_CHECKBOX.addEventListener('click',(event) => {
+    event.preventDefault();
+});
 
 const HREF_LINK = document.querySelector("#href");
-HREF_LINK.addEventListener('click', clica);
-function clica(event){
+HREF_LINK.addEventListener('click',(event) => {
     event.preventDefault();
-}
+});
