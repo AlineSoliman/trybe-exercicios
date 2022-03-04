c222064b200c9e3c36f7ef0eecfc206a27ac47bf
Aline Soliman <alineapreis@hotmail.com>
2022-03-04 15:20:22 -0300

    colocando try/catch para capturar o erro e tratá-lo

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/script.js
@@ -10,11 +10,29 @@
   
 //   console.log(sum(2, '3'));
 
-const sum = (value1, value2) => {
+// const sum = (value1, value2) => {
+//     if (typeof value1 !== 'number' || typeof value2 !== 'number') { 
+//       throw new Error('Os valores devem ser numéricos'); // throw para interromper o funcionamento do código e aparecer o erroo no console.
+//     }
+//     return value1 + value2;
+//   };
+  
+//   console.log(sum(2, '3'));
+
+const verifyIsNumber = (value1, value2) => {
     if (typeof value1 !== 'number' || typeof value2 !== 'number') {
       throw new Error('Os valores devem ser numéricos');
     }
-    return value1 + value2;
+  };
+  
+  const sum = (value1, value2) => {
+    try {
+      verifyIsNumber(value1, value2);
+      return value1 + value2;
+    } catch (error) {
+      throw error.message;
+    }
   };
   
   console.log(sum(2, '3'));
+
