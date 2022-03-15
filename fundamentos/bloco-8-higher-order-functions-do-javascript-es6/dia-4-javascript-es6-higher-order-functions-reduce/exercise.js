4c5ff091926751d2e51ecd95c820b087a620c5c9
Aline Soliman <alineapreis@hotmail.com>
2022-03-15 14:42:04 -0300

    adiciona exercício 1 reduce

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise.js
@@ -1,5 +1,8 @@
 const arrays = [["1", "2", "3"], [true], [4, 5, 6]];
 
+// 1 - Dada uma matriz, transforme em um array.
+
 function flatten() {
-  // escreva seu código aqui
+  return arrays.reduce((acc, curr) => acc.concat(curr), []); // acc "acmulador" e curr "valor atual" concat para concatenar
 }
+console.log(flatten());
