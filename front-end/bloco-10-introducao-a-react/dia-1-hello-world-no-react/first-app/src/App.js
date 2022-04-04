87b21173720df382c6894761a956950d2db84521
Aline Soliman <alineapreis@hotmail.com>
2022-04-04 16:13:11 -0300

    primeiro exercício react

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/first-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import './App.css';
+
+const array = ['aprender React', 'praticar inglês', 'fazer exercícios', 'continuar ontrack na Trybe', 'fazer todos os projetos' ];
+const lista = array.map((element) => <li>{element}</li>);
+const Task = (value) => {
+  return value;
+  }
+
+function App() {
+  return Task(lista) ;
+}
+
+export default App;
